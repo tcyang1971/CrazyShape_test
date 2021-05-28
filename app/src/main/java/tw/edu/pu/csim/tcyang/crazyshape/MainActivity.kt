ac1127bcb700e38fa264548f7fe43f9a16446d19
tcyang <tcyang@gm.pu.edu.tw>
2021-05-28 10:58:10 +0800

    上機考v2_楊子青

--- app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/crazyshape/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import kotlinx.android.synthetic.main.activity_main.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Toast.makeText(baseContext, "作者：tcyang", Toast.LENGTH_LONG).show()
+
+        GlideApp.with(this)
+            .load(R.drawable.cover)
+            .override(800, 600)
+            .into(imgTitle)
+
 
         imgNext.setOnLongClickListener(object : View.OnLongClickListener {
             override fun onLongClick(p0: View?): Boolean {
